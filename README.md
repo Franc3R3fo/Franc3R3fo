d2af474ea278bfa1e4e2006f6a536d35efacedeb
windflaag <francesco.refolli@gmail.com>
2022-03-23 18:28:50 +0100

    generate.py and its own

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
+# windflaag
+
+### Bio
 - 👋 Hi, I’m Francesco Refolli
 - 👀 I’m interested in Computer Science
-- 🌱 I’m currently learning Informatica@Bicocca
+- 🌱 I’m currently on 2° Year of Informatica Triennale @ Milano Bicocca
 - 💞️ I’m looking to collaborate on Java Class Library in order to add nextChar() in Scanner class
 
-active projects:
+### Languages i know
+![C](https://img.shields.io/badge/-C-green.svg) ![C++](https://img.shields.io/badge/-C++-lightblue.svg) ![JAVA](https://img.shields.io/badge/-JAVA-blue.svg) ![PYTHON](https://img.shields.io/badge/-PYTHON-orange.svg) ![PROLOG](https://img.shields.io/badge/-PROLOG-blue.svg) ![COMMON%20LISP](https://img.shields.io/badge/-COMMON%20LISP-green.svg)
 
-| project | description | languages | current sprint deadline |
-| ------- | ----------- | --------- | ----------------------- |
-| nord | a dialect of lisp with its REPL interpreter | C++ | mid april 2022 |
+### OS i prefer
+![FEDORA](https://img.shields.io/badge/-FEDORA-orange.svg) ![ARCH](https://img.shields.io/badge/-ARCH-red.svg) ![DEBIAN](https://img.shields.io/badge/-DEBIAN-orange.svg) ![GENTOO](https://img.shields.io/badge/-GENTOO-orange.svg) ![SLACKWARE](https://img.shields.io/badge/-SLACKWARE-blue.svg)
 
-future projects:
+### Editors i use
+![NOTEPAD++](https://img.shields.io/badge/-NOTEPAD++-lightblue.svg) ![VIM](https://img.shields.io/badge/-VIM-yellow.svg) ![GNU%20EMACS](https://img.shields.io/badge/-GNU%20EMACS-violet.svg) ![MICRO](https://img.shields.io/badge/-MICRO-lightyellow.svg)
 
-| project | description | languages | sprint starting |
-| ------- | ----------- | --------- | --------------- |
-| panzer | control version system | C++ | may/june |
+### My Github statistics
+
+<center>
+<table>
+    <tr>
+        <td><img width="500px" align="left" src="https://github-readme-stats.vercel.app/api?username=windflaag&show_icons=true&theme=tokyonight" /></td>
+        <td><img width="450px" align="left" src="https://github-readme-stats.vercel.app/api/top-langs/?username=windflaag&layout=compact&langs_count=12&theme=tokyonight"/></td>
+    </tr>
+    </table>
+</center>
+
+<p align="center">
+
+![GRAPH](https://activity-graph.herokuapp.com/graph?username=windflaag&hide_border=true&theme=redical)
+
+</p>
+        
+### Active projects
+|project | description | languages | current sprint deadline|
+|------- | ----------- | --------- | -----------------------|
+|nord | a dialect of lisp with its REPL interpreter | C++ | mid april 2022|
+|concurrency | experiments about concurrency | C++ | mid april 2022|
+
+### Future projects
+|project | description | languages | sprint starting|
+|------- | ----------- | --------- | ---------------|
+|panzer | control version system | C++ | may/june|
+
+<center> 2022-03-23 - Generated with windflaag::generate.py </center>
---
+++ generate.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+
+import random, datetime
+
+class Colors:
+    COLORS = [
+        "lightgreen",
+        "green",
+        "lightyellow",
+        "yellow",
+        "orange",
+        "lightred",
+        "red",
+        "lightviolet",
+        "violet",
+        "lightblue",
+        "blue",
+        "lightgrey",
+        "grey"
+    ]
+    
+    @staticmethod
+    def random_color(start=None, end=None):
+        if (end == None):
+            end = len(Colors.COLORS)
+        if (start == None):
+            start = 0
+        return random.choice(Colors.COLORS[start : end+1])
+    
+    @staticmethod
+    def random_colors(length, start=None, end=None):
+        return [ Colors.random_color(start, end) for _ in range(length) ]
+    
+    @staticmethod
+    def sequence_colors(length, start=None):
+        if (start == None):
+            start = 0
+        return Colors.COLORS[start : start+length]
+
+class Badges:
+    @staticmethod
+    def getZERO(thing, color):
+        return f"![{thing}](https://img.shields.io/badge/-{thing}-{color}.svg)"
+
+class Generators:
+    @staticmethod
+    def getZERO(conf):
+        things = conf["things"]
+        colors = conf["colors"]
+        header = conf["header"]
+        return f"\n### {header}\n{' '.join([ Badges.getZERO(thing, color) for (thing, color) in zip(things, colors) ])}\n"
+    
+    @staticmethod
+    def getUNO(conf):
+        username = conf["username"]
+        header = conf["header"]
+        THEME_1 = ""
+        THEME_2 = ""
+        THEME_2 = ""
+        if ("theme_1" in conf):
+            THEME_1 = f"&theme={conf['theme_1']}"
+        if ("theme_2" in conf):
+            THEME_2 = f"&theme={conf['theme_2']}"
+        if ("theme_3" in conf):
+            THEME_3 = f"&theme={conf['theme_3']}"
+        
+        HIDE = ""
+        if len(conf["hide"])>0:
+            HIDE=f"&hide={','.join(conf['hide'])}"
+        
+        EXCLUDE_REPO = ""
+        if len(conf["exclude_repo"])>0:
+            EXCLUDE_REPO=f"&exclude_repo={','.join(conf['exclude_repo'])}"
+        
+        return """\n### """+f"{header}"+"""
+
+<center>
+<table>
+    <tr>
+        """+f"<td><img width=\"500px\" align=\"left\" src=\"https://github-readme-stats.vercel.app/api?username={username}&show_icons=true{THEME_1}\" /></td>"+"""
+        """+f"<td><img width=\"450px\" align=\"left\" src=\"https://github-readme-stats.vercel.app/api/top-langs/?username={username}&layout=compact&langs_count=12{THEME_2}{EXCLUDE_REPO}{HIDE}\"/></td>"+"""
+    </tr>
+    </table>
+</center>
+
+<p align="center">
+
+"""+f"![GRAPH](https://activity-graph.herokuapp.com/graph?username={username}&hide_border=true{THEME_3})"+"""
+
+</p>
+        """
+    @staticmethod
+    def getDUE(conf):
+        header = conf["header"]
+        columns = conf["columns"]
+        rows = conf["rows"]
+        ss = """\n### """ + f"{header}\n" + "|" + " | ".join(columns) + "|\n" + "|" + " | ".join([ "-"*len(col) for col in columns ]) + "|\n"
+        for row in rows:
+            ss += "|" + " | ".join([ col for col in row ]) + "|\n"
+        return ss
+    
+    @staticmethod
+    def getTRE(conf):
+        header = conf["header"]
+        username = conf["username"]
+        payload = conf["payload"]
+        return f"# {username}\n\n" + f"### {header}\n"  + f"{payload}\n"
+    
+    @staticmethod
+    def getWATERMARK():
+        return f"\n<center> {datetime.date.today()} - Generated with windflaag::generate.py </center>\n"
+
+config = {
+    "BIO": {
+        "type": "TRE",
+        "username": "windflaag",
+        "header": "Bio",
+        "payload": """- 👋 Hi, I’m Francesco Refolli
+- 👀 I’m interested in Computer Science
+- 🌱 I’m currently on 2° Year of Informatica Triennale @ Milano Bicocca
+- 💞️ I’m looking to collaborate on Java Class Library in order to add nextChar() in Scanner class"""
+    },
+    "LANGUAGES": {
+        "type": "ZERO",
+        "header": "Languages i know",
+        "things": ["C", "C++", "JAVA", "PYTHON", "PROLOG", "COMMON%20LISP"],
+        "colors": Colors.random_colors(6)
+    },
+    "OS": {
+        "type": "ZERO",
+        "header": "OS i prefer",
+        "things": ["FEDORA", "ARCH", "DEBIAN", "GENTOO", "SLACKWARE"],
+        "colors": Colors.random_colors(5)
+    },
+    "EDITORS": {
+        "type": "ZERO",
+        "header": "Editors i use",
+        "things": ["NOTEPAD++", "VIM", "GNU%20EMACS", "MICRO"],
+        "colors": Colors.random_colors(4)
+    },
+    "STATISTICS": {
+        "header": "My Github statistics",
+        "type": "UNO",
+        "username": "windflaag",
+        "hide": [],
+        "exclude_repo": [],
+        "theme_1": "tokyonight",
+        "theme_2": "tokyonight",
+        "theme_3": "redical"
+    },
+    "ACTIVE_PROJECTS": {
+        "type": "DUE",
+        "header": "Active projects",
+        "columns": ["project", "description", "languages", "current sprint deadline"],
+        "rows": [
+            ["nord", "a dialect of lisp with its REPL interpreter", "C++", "mid april 2022"],
+            ["concurrency", "experiments about concurrency", "C++", "mid april 2022"]
+        ]
+    },
+    "FUTURE_PROJECTS": {
+        "type": "DUE",
+        "header": "Future projects",
+        "columns": ["project", "description", "languages", "sprint starting"],
+        "rows": [ ["panzer", "control version system", "C++", "may/june"] ],
+    }
+}
+
+def main():
+    text = ""
+    for obj in config:
+        if config[obj]["type"] == "ZERO":
+            text += Generators.getZERO(config[obj])
+        if config[obj]["type"] == "UNO":
+            text += Generators.getUNO(config[obj])
+        if config[obj]["type"] == "DUE":
+            text += Generators.getDUE(config[obj])
+        if config[obj]["type"] == "TRE":
+            text += Generators.getTRE(config[obj])
+    text += Generators.getWATERMARK() # please give me the communist credits for this script
+    
+    file = open("README.md", "w")
+    file.write(text)
+    file.close()
+
+main()
